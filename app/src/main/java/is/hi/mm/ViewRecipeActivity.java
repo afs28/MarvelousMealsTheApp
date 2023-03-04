54b562e262fed4a395d074726ba10c9c47d08baf
hebasolveig <hsh58@hi.is>
2023-03-04 16:20:01 +0000

    bætti við tilbaka-ör efst

--- app/src/main/java/is/hi/mm/ViewRecipeActivity.java
+++ app/src/main/java/is/hi/mm/ViewRecipeActivity.java
@@ -3,6 +3,7 @@ package is.hi.mm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ViewRecipeActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class ViewRecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_recipe);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        // Handle the back button click event
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
