46905d93aa15cfed8f8d58c38abda4823556df7a
hebasolveig <hsh58@hi.is>
2023-03-04 15:19:38 +0000

    naði að birta title :))

--- app/src/main/java/is/hi/mm/MainActivity.java
+++ app/src/main/java/is/hi/mm/MainActivity.java
@@ -1,6 +1,7 @@
 package is.hi.mm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -16,16 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    /*
-
-    Dæni/Note to self:
-    private Button mButtonTrue
-    private Button mButtonFalse
-    muna eftir ad gefa hlutum ID
-    allur texti er skilgreindur i strings.xml
-    etc
-
-    */
+    private RecyclerView mRecipeListRecyclerView;
+    private RecipeAdapter mAdapter;
 
     private List<Recipe> mRecipeList;
 
@@ -34,41 +27,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //viljum sækja af bakendanum
+        mRecipeListRecyclerView = findViewById(R.id.mRecipeList);
+        mRecipeListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         NetworkManager networkManager = NetworkManager.getInstance(this);
-        networkManager.getRecipes(new NetworkCallback<List<Recipe>> () {
+        networkManager.getRecipes(new NetworkCallback<List<Recipe>>() {
             @Override
             public void onSuccess(List<Recipe> result) {
-                //hvað viljum við gera þegar við fáum listann af Recipes.
-                //viljum setja það í eitthvað eins og RecyclerView
                 mRecipeList = result;
+                mAdapter = new RecipeAdapter(MainActivity.this, mRecipeList);
+                mRecipeListRecyclerView.setAdapter(mAdapter);
+
+                // Accessing the first recipe in the list
+                Recipe firstRecipe = mRecipeList.get(0);
+
+                // Printing the description of the first recipe
+                Log.d(TAG, "Description of the first recipe: " + firstRecipe.getDescription());
             }
+
             @Override
             public void onFailure(String errorString) {
                 Log.e(TAG, errorString);
             }
         });
 
-        /*
-        TODO: Laga RestControllers á hinu verkefninu
-        TODO: Setja upp entities, activities, services, network manager
-        TODO: Læra betur um fragments
-        TODO: Setja upp strengi
-        TODO: Skoða útlit fyrir app
-        TODO: Sækja libraries til að meðhöndla myndir ofl
-        TODO: Horfa a restina af support videos
-
-        mButtonTrue = (Button) findViewById(R.id.true_button td
-        mButtonTrue.setOnClickListener(new View.OnClickListener() >> android studio klarar
-        at override
-        public void onClick(View v) {
-                    logic
-                    Toast.makeText(MainActivity.this, R.string.toast_correct, Toast.LENGTH_SHORT).show()
-                    etc
-        }
-
-         */
-
+        // TODO: Add code for button onClick listener and other TODO items.
     }
-
 }
---
+++ app/src/main/java/is/hi/mm/RecipeAdapter.java
@@ -0,0 +1,81 @@
+package is.hi.mm;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import is.hi.mm.entities.Recipe;
+import is.hi.mm.networking.NetworkCallback;
+import is.hi.mm.networking.NetworkManager;
+import is.hi.mm.R;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+
+    private Context mContext;
+    private List<Recipe> mRecipeList;
+
+    public RecipeAdapter(Context context, List<Recipe> recipeList) {
+        mContext = context;
+        mRecipeList = recipeList;
+    }
+
+    private void loadRecipes() {
+        NetworkManager.getInstance(mContext).getRecipes(new NetworkCallback<List<Recipe>>() {
+            @Override
+            public void onSuccess(List<Recipe> result) {
+                mRecipeList = result;
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(String errorString) {
+                // handle error
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_item, parent, false);
+        RecipeViewHolder viewHolder = new RecipeViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+        Recipe recipe = mRecipeList.get(position);
+        holder.bindRecipe(recipe);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mRecipeList == null) {
+            return 0;
+        } else {
+            return mRecipeList.size();
+        }
+    }
+
+    class RecipeViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mRecipeNameTextView;
+
+        public RecipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mRecipeNameTextView = itemView.findViewById(R.id.recipe_name_text_view);
+        }
+
+        public void bindRecipe(Recipe recipe) {
+            mRecipeNameTextView.setText(recipe.getTitle());
+        }
+    }
+
+}
